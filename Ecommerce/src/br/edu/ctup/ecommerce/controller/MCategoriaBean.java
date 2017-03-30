e67fd1d9a93ce3267e770f8f014c04e37c4f5c17
lftis <lftis@DESKTOP-ITAUQMP>
2017-03-30 16:53:10 -0300

    fsfsdfsdsdf

---
+++ Ecommerce/src/br/edu/ctup/ecommerce/controller/MCategoriaBean.java
@@ -0,0 +1,31 @@
+package br.edu.ctup.ecommerce.controller;
+
+import javax.faces.bean.ManagedBean;
+
+import br.edu.ctup.ecommerce.model.Categoria;
+
+@ManagedBean(name="mCategoriaBean")
+public class MCategoriaBean {
+	
+	private Categoria categoria = new Categoria();
+	
+	
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+
+
+	public void gravarCategoria(Categoria c) {
+		//CategoriaDAO.adicionarCategoria(c);
+		System.out.println("nome: " + c.getNome() + " " + c.getDesc());
+	}
+	
+}
